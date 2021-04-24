8180de4d768969332e41acc6930339aeae1bc22a
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-04-24 21:38:31 +0200

    first commit

---
+++ src/test/java/pl/tomaszosuch/bikeshop/BikeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.bikeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
